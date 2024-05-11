be8cc2657878abf234cfaa178bdc0eb1e29e7967
Alex H <alex.holder@bigfootds.com>
2024-05-11 10:27:14 +1000

    feat: component router with absolute paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import AboutPage from './pages/About'
 import ContactPage from './pages/Contact'
@@ -9,9 +10,13 @@ function App() {
 
   return (
     <>
-      <HomePage />
-      <AboutPage />
-      <ContactPage />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/about' element={<AboutPage />} />
+          <Route path='/contact' element={<ContactPage />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
