073d76cf721ccb86787485ac8893a83b7d968caf
Alex H <alex.holder@bigfootds.com>
2024-05-11 11:48:18 +1000

    feat: contact page has redirect button

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,3 +1,24 @@
-export default function ContactPage(){
-	return (<h1>Contact page</h1>);
+import {Component} from "react";
+import { Navigate } from "react-router-dom";
+
+export default class ContactPage extends Component {
+	constructor(){
+		super();
+
+		this.state = {
+			shouldNavigate: false
+		}
+	}
+
+	updateShouldNavigate(){
+		this.setState({shouldNavigate: true});
+	}
+
+	render(){
+		return (<>
+			{this.state.shouldNavigate && <Navigate to={"/"} />}
+			<h1>Contact page</h1>
+			<button onClick={() => this.setState({shouldNavigate: true})}>Go home</button>
+		</>);
+	}
 }
