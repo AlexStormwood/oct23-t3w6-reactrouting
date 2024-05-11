03a4210e06a5c7c200af3073ab0714c3131e2145
Alex H <alex.holder@bigfootds.com>
2024-05-11 10:35:52 +1000

    feat: nested and 404 routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 import AboutPage from './pages/About'
 import ContactPage from './pages/Contact'
 import HomePage from './pages/Home'
+import PageNotFound from './pages/PageNotFound'
 
 function App() {
   
@@ -13,8 +14,17 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<HomePage />} />
-          <Route path='/about' element={<AboutPage />} />
+          <Route path='/about/services' element={<AboutPage />} />
           <Route path='/contact' element={<ContactPage />} />
+          {/* Need a page for any not-matched route */}
+          <Route path='*' element={<PageNotFound />} /> 
+
+          {/* <Route path='/' element={<HomePage />}>
+            <Route path='about'>
+              <Route path='services' element={<AboutPage />} />
+            </Route>
+            <Route path='contact' element={<ContactPage />} />
+          </Route> */}
         </Routes>
       </BrowserRouter>
     </>
